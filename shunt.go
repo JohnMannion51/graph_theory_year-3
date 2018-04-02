fe05c746335e50f140f23d9ed8ec4f857ff87fe8
jono mannion <johnmannion51@gmail.com>
2018-04-02 16:38:18 +0100

    video 2 shunt

---
+++ shunt.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)// imports
+
+func intopost(infix string) string{
+	specials := map[rune]int{'*': 10,'.': 9,'|': 8}
+
+	pofix , s:= []rune{}, []rune{}
+
+	for _, r := range infix {
+		switch {
+		case r == '(':
+		s = append(s, r)
+		//case (
+		case r == ')':
+			for s[len(s)-1] != '(' {
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}// for
+		s = s[:len(s)-1]
+		//case )
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}// for
+			s = append(s,r)
+		//case specials[r]
+		default:
+			pofix = append(pofix,r)
+		}// switch
+
+	}// for
+	for len(s) > 0 {
+		pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+	}// for
+
+	return string(pofix)
+}// intopost
+
+func main(){
+	// Answer: ab.c*.
+	fmt.Println("Infix:   ", "a.b.c*")
+	fmt.Println("Postfix: ", intopost("a.b.c*"))
+
+	// Answer: abd|.*
+	fmt.Println("Infix:   ", "(a.(b|d))*")
+	fmt.Println("Postfix: ", intopost("(a.(b|d))*"))
+
+	// Answer: abd|.c*.
+	fmt.Println("Infix:   ", "a.(b|d).c*")
+	fmt.Println("Postfix: ", intopost("a.(b|d).c*"))
+
+	// Answer: abb.+.c.
+	fmt.Println("Infix:   ", "a.(b.b)+.c")
+	fmt.Println("Postfix: ", intopost("a.(b.b)+.c"))
+
+}
